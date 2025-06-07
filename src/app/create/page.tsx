fef0854b727ff84fe2d9535651bc1f69e8cdd9c7
jiebinzhou98 <benzhou98@outlook.com>
2025-06-07 14:27:09 -0400

    feat: :zap: implement more tiptap on create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,14 +1,54 @@
 'use client'
 
-import Editor from "@/components/ui/Editor";
+import { useState } from "react";
 import { supabase } from "@/lib/supabase";
 import dynamic from "next/dynamic";
 
+//Editor 已经是csr了，为了不跟next.js自带的dynamic rendering冲突
+const Editor = dynamic (() => import ('@/components/ui/Editor'), {ssr: false})
+
 export default function CreatePage() {
+  const [title, setTitle] = useState("")
+  const [editorContent, setEditorContent] = useState<any>(null)
+  const [status, setStatus] = useState("")
+
+  const handleSave = async () =>{
+    setStatus("Saving...")
+
+    const {data, error} = await supabase.from("notes").insert({
+    title,
+    content: editorContent,
+    user_key: "local"
+  })
+
+  if(error){
+    setStatus("Failed to save: " +error.message)
+  }else{
+    setStatus("Note Saved")
+    setTitle("")
+    setEditorContent(null)
+  }
+}
+
   return (
-    <main className="p-4 max-w-2xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Create a New Note</h1>
-      <Editor />
+    <main className="p-4 max-w-2xl mx-auto space-y-4">
+      <h1 className="text-2xl font-bold">Create a New Note</h1>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) =>setTitle(e.target.value)}
+        placeholder="Enter note title"
+        className="w-full border rounded p-2 text-lg"
+      />
+      <Editor onChange={setEditorContent} />
+
+      <button
+        onClick={handleSave}
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
+      >
+        Save Note
+      </button>
+      <p className="text-sm text-gray-500">{status}</p>
     </main>
   )
 }
--- src/components/ui/Editor.tsx
+++ src/components/ui/Editor.tsx
@@ -2,15 +2,26 @@
 
 import {useEditor, EditorContent} from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
+import { useEffect } from "react";
 
-export default function Editor() {
+export default function Editor({onChange}: {onChange?: (json:any) =>void}) {
     const editor = useEditor({
         extensions:[StarterKit],
-        content: "<p>Start wrtiting your note here...",
-    });
+        content: "",
+        onUpdate: ({editor}) => {
+            const json = editor.getJSON()
+            onChange?.(json)
+        },
+    })
+
+    useEffect(() => {
+        return () =>{
+            editor?.destroy()
+        }
+    },[editor])
 
     return(
-        <div className="border rounded-md shadow-sm p-4 min-h-[300px]">
+        <div className="border rounded p-3 min-h-[200px]">
             <EditorContent editor={editor}/>
         </div>
     )
